406f9b9987c17f628b0e3200516e900ed6635df5
Saurabh Thakur <saurabh.thakur@climatepartner.com>
2023-01-23 15:24:22 +0100

    middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 
 import { CatsModule } from './cats/cats.module';
+import { HealthController } from './health/health.controller';
 import { HealthModule } from './health/health.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule, HealthModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET }, HealthController);
+  }
+}
